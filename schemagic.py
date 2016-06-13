229eed25f82f9f29323acdaf49448cc4c83d95e7
Tyler <tjtolton@gmail.com>
2016-06-13 17:39:29 -0400

    implemented schemagic base functionality

---
+++ schemagic.py
@@ -0,0 +1,56 @@
+import collections
+from utils import merge_with, assert_raises, multiple_dispatch_fn
+
+def validate_map_template(schema, value):
+    key_schema, value_schema = schema.items()[0]
+    validate_key_val_pair = lambda key, val: (validate_against_schema(key_schema, key), validate_against_schema(value_schema, val))
+    return dict(map(validate_key_val_pair, value.keys(), value.values()))
+
+def validate_keyed_mapping(schema, value):
+    missing_keys = set(schema.keys()) - set(value.key())
+    if missing_keys:
+        raise ValueError("Missing keys {0} for value {1}".format(missing_keys, value))
+    return merge_with(validate_against_schema, schema, value)
+
+def validate_sequence_template(schema, value):
+    return map(schema[0], value)
+
+def validate_strict_sequence(schema, value):
+    if not len(schema) == len(value):
+        raise ValueError("sequence has a different number of elements than its schema perscribes.  value: {0}, schema: {1}".format(value, schema))
+    return map(lambda x, y: x(y), schema, value)
+
+is_map_template = lambda schema: isinstance(schema, collections.MutableMapping) and len(schema.items()) is 1 and not isinstance(schema.keys()[0], str)
+is_keyed_mapping = lambda schema: isinstance(schema, collections.MutableMapping) and not is_map_template(schema)
+is_sequence_template = lambda schema: isinstance(schema, collections.Sequence) and len(schema) is 1
+is_strict_sequence = lambda schema: isinstance(schema, collections.Sequence) and 1 < len(schema)
+
+validate_against_schema = multiple_dispatch_fn({
+        is_sequence_template: validate_sequence_template,
+        is_strict_sequence: validate_strict_sequence,
+        is_map_template: validate_map_template,
+        is_keyed_mapping: validate_keyed_mapping},
+    default=lambda schema, value: schema(value))
+
+def test_validate_against_schema():
+    # test base situation, a simple function schema
+    assert validate_against_schema(str, "hello")
+    with assert_raises(ValueError):
+        validate_against_schema(int, "hello")
+
+    # test map template
+    assert validate_against_schema({int: str}, {1: "hello", 2: "world"})
+    with assert_raises():
+        validate_against_schema({int: str}, {"not an int": "hello", "definitely not an int": "world"})
+
+    # test sequence template
+    assert validate_against_schema([int], [1, 2, 3])
+    with assert_raises():
+        validate_against_schema([int], ["not an int", 1, "is definitely not an int"])
+
+    # test strict sequence
+    assert validate_against_schema([int, int], [1, 2])
+    with assert_raises():
+        validate_against_schema([int, int], [1])
+
+test_validate_against_schema()
---
+++ utils.py
@@ -0,0 +1,80 @@
+import copy
+import doctest
+from contextlib import contextmanager
+from functools import wraps
+
+import operator
+
+import itertools
+
+
+def merge_with(fn, a, b):
+    """ returns a new dictionary that is the merger of a and b.  applies fn to the values of colliding keys.
+
+    >>> merge_with(operator.add, {"a": 1}, {"a": 1, "b": 1})
+    {'a': 2, 'b': 1}
+    """
+    fresh_dict = copy.deepcopy(a)
+    for k, v in b.items():
+        if k in fresh_dict:
+            fresh_dict[k] = fn(fresh_dict[k], v)
+        else:
+            fresh_dict[k] = v
+    return fresh_dict
+
+def multiple_dispatch_fn(dispatch_map, default=None):
+    """
+    creates a multiple dispatch function.
+
+    returns a function whose implementation of the function is based on the arguments passed to it.  it decides
+    what implementation to use by testing the arguments against a series of predicates to detect what situation
+    is applicable.
+
+    For example,
+    >>> add_if_ints_multiply_if_floats = multiple_dispatch_fn(
+    ... {lambda *nums: all(isinstance(num, int) for num in nums): operator.add,
+    ...  lambda *nums: all(isinstance(num, float) for num in nums): operator.mul})
+    >>> add_if_ints_multiply_if_floats(10, 10)
+    20
+    >>> add_if_ints_multiply_if_floats(10.0, 10.0)
+    100.0
+
+    You can also provide a default implementation to use if none of the predicates match.
+    For example,
+    >>> add_if_ints_else_return_unmodified = multiple_dispatch_fn(
+    ... {lambda *nums: all(isinstance(num, int) for num in nums): operator.add},
+    ...  default=lambda *items: items)
+    >>> add_if_ints_else_return_unmodified(25, 25)
+    50
+    >>> add_if_ints_else_return_unmodified("hello", None)
+    ('hello', None)
+
+    :param dispatch_map: mapping {predicate_fn: implementation_fn}
+    :param default: implementation fn to be used if none of the predicates are satisfied
+    :return:
+    """
+    dispatch_map = dispatch_map or {}
+    def _fn(*args, **kwargs):
+        try:
+            dispatch_fn = next(itertools.chain([value for key, value in dispatch_map.items() if key(*args, **kwargs)],
+                                               [default] if default else []))
+            return dispatch_fn(*args, **kwargs)
+        except StopIteration:
+            raise ValueError("No dispatch function found. args: {0}, kwargs: {1}".format(args, kwargs))
+    return _fn
+
+@contextmanager
+def assert_raises(expected_error=None):
+    try:
+        yield
+    except Exception as e:
+        if expected_error:
+            assert isinstance(e, expected_error), "Expected to raise type(s): {0}, raised {1} instead".format(expected_error, e.__class__.__name__)
+        else:
+            # No specific error raised, so we're good.
+            pass
+    else:
+        raise AssertionError("No exception raised")
+
+if __name__ == '__main__':
+    doctest.testmod()
