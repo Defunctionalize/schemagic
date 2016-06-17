50bf43b9d7786f66b0ce104f3a172df20186d661
Tyler <tjtolton@gmail.com>
2016-06-17 11:43:46 -0400

    tested webservice route functionality

--- schemagic/web.py
+++ schemagic/web.py
@@ -32,9 +32,10 @@ def webservice_fn(fn, validation_predicate, input_validator, output_validator):
 
 def service_route(service, validation_pred=None, coerce_data=True, rule=None, input_schema=None, output_schema=None, fn=None):
     if not rule:
-        raise ValueError("rule must be defined")
+        return partial(service_route, service, validation_pred, coerce_data)
     if fn is None:
-        return partial(service_route, validation_pred, coerce_data, input_schema, output_schema)
+        return partial(service_route, service, validation_pred, coerce_data, rule, input_schema, output_schema)
+
     validation_pred = validation_pred or WHEN_DEBUGGING
     input_validator = validator(input_schema or IDENTITY, "input to endpoint {0}".format(rule), coerce_data)
     output_validator = validator(output_schema or IDENTITY, "output from endpoint {0}".format(rule), coerce_data)
@@ -42,7 +43,9 @@ def service_route(service, validation_pred=None, coerce_data=True, rule=None, in
     service.add_url_rule(
         rule=rule,
         endpoint=fn.__name__ if hasattr(fn, "__name__") else rule,
-        view_func=update_wrapper(webservice_fn(fn, validation_pred, input_validator, output_validator), fn),
+        view_func=update_wrapper(lambda: webservice_fn(fn, validation_pred, input_validator, output_validator), fn),
         methods=['POST']
     )
     return fn
+
+
