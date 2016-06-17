0dfd705bb8db4b19bdb01c443b804ea6f5e7ed27
Tyler <tjtolton@gmail.com>
2016-06-17 12:51:10 -0400

    added setup file to repo to assist in publishing

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+NAME = 'schemagic'
+PACKAGES = ['schemagic']
+VERSION = '0.2.0'
+LICENSE = "GPL"
+DESCRIPTION = 'Define the shape of your data with simple python data structures. Use those data descriptions to validate your application.'
+AUTHOR = 'Tyler Tolton'
+AUTHOR_EMAIL = 'tjtolton@gmail.com'
+URL = 'https://github.com/TJTolton/schemagic'
+KEYWORDS = ['schema', 'schemas', 'schemata',
+          'validate', 'validation', 'validator'
+          'json', 'REST', 'webservice', 'flask', 'POST'
+          'agile']
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        keywords=KEYWORDS,
+        packages=PACKAGES,
+        zip_safe=False,
+    )
