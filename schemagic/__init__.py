17798e5f6aae5da633a77ef0a3d056099b44affd
Tyler Tolton <tjtolton@gmail.com>
2016-06-24 16:30:32 -0400

    add sphinx generated documentation (#11)

--- schemagic/__init__.py
+++ schemagic/__init__.py
@@ -1,1 +1,9 @@
 from schemagic.core import *
+
+
+__version__ = '0.3.24'
+__author__ = 'Tyler Tolton'
+__authoremail__ = 'tjtolton@gmail.com'
+__url__ = 'https://github.com/Mechrophile/schemagic'
+__license__ = "LGPL"
+__description__ = 'Define the shape of your data with simple python data structures. Use those data descriptions to validate your application.'
