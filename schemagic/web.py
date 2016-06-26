f641df7f8ee4eaefc9e2df010acd348e8299c5d8
Tyler <tjtolton@gmail.com>
2016-06-26 16:25:57 -0400

    tighten up documentation for utils and web

--- schemagic/web.py
+++ schemagic/web.py
@@ -9,24 +9,25 @@ from flask.wrappers import Response
 from schemagic.core import validate_against_schema, validator
 from schemagic.utils import multiple_dispatch_fn
 
-ALWAYS = lambda: True
-WHEN_DEBUGGING = lambda: __debug__
-IDENTITY = lambda x: x
+_ALWAYS = lambda: True
+_WHEN_DEBUGGING = lambda: __debug__
+_IDENTITY = lambda x: x
 
 _dispatch_to_fn = multiple_dispatch_fn({
     lambda fn, args: isinstance(args, basestring): lambda fn, arg_list: fn(arg_list),
     lambda fn, args: isinstance(args, collections.Sequence): lambda fn, arg_list: fn(*arg_list),
     lambda fn, args: isinstance(args, collections.MutableMapping): lambda fn, arg_list: fn(**arg_list)},
-    default= lambda fn, arg_list: fn(arg_list)
+    default=lambda fn, arg_list: fn(arg_list)
 )
 dispatch_to_fn = lambda fn, args: _dispatch_to_fn(fn, args)
 dispatch_to_fn.__doc__ = \
     """Dispatches a json object to a function.  The way the data is applied depends on the structure of the data.
-    #. if the data is a sequence, it will unpack it and pass each item into the function, i.e. it will use *args
-    #. if the data is a mapping, it will unpack it and pass in the items as keywords, i.e. it will use **kwargs
-    #. if the data is anything else (i.e. it is a primitive, non iterable), it will pass it in directly.
+        * if the data is a sequence, it will unpack it and pass each item into the function, i.e. it will use ``*args``
+        * if the data is a mapping, it will unpack it and pass in the items as keywords, i.e. it will use ``**kwargs``
+        * if the data is anything else (i.e. it is a primitive, non iterable), it will pass it in directly.
 
-    **NOTE** an important "gotcha" of this implementation is that a function that expects a single, iterable object
+    **NOTE**
+        an important "gotcha" of this implementation is that a function that expects a single, iterable object
         will have to have its argument passed to it by keyword.  This causes a lot builtin functions in earlier versions
         of python to be ineligible for this kind of dispatch.  For instance, the sum function in 2.7 takes a single
         iterable argument, and that argument can not be passed by keyword.  as such, this function can not be used
@@ -57,8 +58,8 @@ def webservice_fn(fn, input_validator, output_validator):
     """Handles the minutia of pulling data from the request object and passing it into the function and validators
 
     :param fn: the function which is supposed to fulfill the contract defined by the input and output schemata
-    :param input_validator: a `validator` as described in the core function of the same name
-    :param output_validator: a `validator` as described in the core function of the same name
+    :param input_validator: a ``validator`` as described in the core function of the same name
+    :param output_validator: a ``validator`` as described in the core function of the same name
     :return: a json Flask Response that contains either the requested data or an error.
     """
     try:
@@ -78,37 +79,38 @@ def webservice_fn(fn, input_validator, output_validator):
 
 
 def service_route(service, validation_pred=None, coerce_data=True, rule=None, input_schema=None, output_schema=None, fn=None):
-    """Function decorator that registers a `webservice_fn` version of the function on the provided service.
+    """Function decorator that registers a ``webservice_fn`` version of the function on the provided service.
 
-    Note that this function is used primarily to register functions en masse with the `service_registry` interface.
+    Note that this function is used primarily to register functions en masse with the ``service_registry`` interface.
     However, it can be used as a traditional decorator if desired. e.g.:
 
-    my_service_route = partial(service_route, my_service)
+    .. code_block:: python
 
-    @my_service_route(rule="/this-route", input_schema=[int], output_schema=int)
-    def my_sum(*ints):
-        return sum(ints)
+        my_service_route = partial(service_route, my_service)
+
+        @my_service_route(rule="/this-route", input_schema=[int], output_schema=int)
+        def my_sum(*ints):
+            return sum(ints)
 
     I find there to be 2 important pitfalls to bear in mind if using the decorator this way:
-    #. This makes it seem like the function is being somehow modified, which it is not.
-        It can confuse people reading your code into thinking they have to make separate, dedicated webservice versions
-        of the function in order to register them.
-    #. It becomes unsafe to use positional arguments when using the decorator like this.  If I had defined that decorator
-        using the canonical flask pattern, e.g. @my_service_route("/this-route") it would have caused everything to
-        explode.  To get consistent behavior, you MUST specify the arguments by keyword.
+    1. This makes it seem like the function is being somehow modified, which it is not.
+       It can confuse people reading your code into thinking they have to make separate, dedicated webservice versions
+       of the function in order to register them.
+    2. It becomes unsafe to use positional arguments when using the decorator like this.  If I had defined that decorator
+       using the canonical flask pattern, e.g. ``@my_service_route("/this-route")`` it would have caused everything to
+       explode.  To get consistent behavior, you MUST specify the arguments by keyword.
 
-    :param service: The service or app which is to have the rule added to it. Must support the `add_url_rule` interface
-        as described in the `flask documentation`_.
-        .. _`flask documentation`: http://flask.pocoo.org/docs/0.11/api/
+    :param service: The service or app which is to have the rule added to it. Must support the ``add_url_rule`` interface
+        as described in the `flask documentation <http://flask.pocoo.org/docs/0.11/api/>`_.
 
-    :param validation_pred: see description in `validator` fn of the same param.  function that returns true or false
-        The default value for validation on webservice routes is to use the value of __debug__ as a guide.
-    :param coerce_data: see description in `validator` fn of the same param.  boolean flag for coercing data.
+    :param validation_pred: see description in ``validator`` fn of the same param.  function that returns true or false
+        The default value for validation on webservice routes is to use the value of ``__debug__`` as a guide.
+    :param coerce_data: see description in ``validator`` fn of the same param.  boolean flag for coercing data.
         The default is to coerce data.  This is often very helpful in parsing json from a web request.
     :param rule: the url route to use when accessing this function
-    :param input_schema: a data definition as decribed in the `validate_against_schema` fn documentation.
+    :param input_schema: a data definition as described in the ``validate_against_schema`` fn documentation.
         This value is not required.  If none is given, no validation will be done on the input.
-    :param output_schema: a data definition as decribed in the `validate_against_schema` fn documentation.
+    :param output_schema: a data definition as decribed in the ``validate_against_schema`` fn documentation.
         This value is not required.  If none is given, no validation will be done on the output.
     :param fn: The function intended to implement the request.
     :return: the original function, unmodified.
@@ -118,9 +120,9 @@ def service_route(service, validation_pred=None, coerce_data=True, rule=None, in
     if fn is None:
         return update_wrapper(partial(service_route, service, validation_pred, coerce_data, rule, input_schema, output_schema), service_route)
 
-    validation_pred = validation_pred or WHEN_DEBUGGING
-    input_validator = validator(input_schema or IDENTITY, "input to endpoint {0}".format(rule), validation_predicate=validation_pred, coerce_data=coerce_data)
-    output_validator = validator(output_schema or IDENTITY, "output from endpoint {0}".format(rule), validation_predicate=validation_pred, coerce_data=coerce_data)
+    validation_pred = validation_pred or _WHEN_DEBUGGING
+    input_validator = validator(input_schema or _IDENTITY, "input to endpoint {0}".format(rule), validation_predicate=validation_pred, coerce_data=coerce_data)
+    output_validator = validator(output_schema or _IDENTITY, "output from endpoint {0}".format(rule), validation_predicate=validation_pred, coerce_data=coerce_data)
 
     service.add_url_rule(
         rule=rule,
@@ -132,11 +134,11 @@ def service_route(service, validation_pred=None, coerce_data=True, rule=None, in
 
 
 def service_registry(service, validation_pred=None, coerce_data=True, *service_definitions):
-    """Registers all the service descriptions provided on the app specified by the `service` parameter.
+    """Registers all the service descriptions provided on the app specified by the ``service`` parameter.
 
-    :param service: Service to register functions on. see description of same parameter in `service_route` documentation.
-    :param validation_pred: function returning boolean. see description of same parameter in `service_route` documentation.
-    :param coerce_data: boolean. see description of same parameter in `service_route` documentation.
+    :param service: Service to register functions on. see description of same parameter in ``service_route`` documentation.
+    :param validation_pred: function returning boolean. see description of same parameter in ``service_route`` documentation.
+    :param coerce_data: boolean. see description of same parameter in ``service_route`` documentation.
     :param service_definitions: mappings that contain the keyword args to service_route.
     :return: if service definitions not provided, returns the a function that accepts service definitions.
             if service definitions provided, returns nothing.
