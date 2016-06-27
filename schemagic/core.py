c232fa0fa6d8acd5f6d100411fba77835d464309
Jay <jatolton@cisco.com>
2016-06-27 19:46:07 -0400

    general cleanup, also.  Fixed bug for validate_sequence_template not working with enum

--- schemagic/core.py
+++ schemagic/core.py
@@ -5,10 +5,13 @@ from schemagic.utils import merge_with, multiple_dispatch_fn
 
 WHEN_DEBUGGING = lambda: __debug__
 
+
 def validate_map_template(schema, value):
     key_schema, value_schema = schema.items()[0]
-    validate_key_val_pair = lambda key, val: (validate_against_schema(key_schema, key), validate_against_schema(value_schema, val))
+    validate_key_val_pair = lambda key, val: (
+    validate_against_schema(key_schema, key), validate_against_schema(value_schema, val))
     return dict(map(validate_key_val_pair, value.keys(), value.values()))
+
 
 def validate_keyed_mapping(schema, value):
     missing_keys = set(schema.keys()) - set(value.keys())
@@ -16,25 +19,34 @@ def validate_keyed_mapping(schema, value):
         raise ValueError("Missing keys {0} for value {1}".format(missing_keys, value))
     return merge_with(validate_against_schema, schema, value)
 
+
 def validate_sequence_template(schema, value):
-    return map(schema[0], value)
+    if hasattr(schema[0], '__call__'):
+        return map(schema[0], value)
+    return map(lambda _: apply(lambda _s, _v: validate_against_schema(_s, _v), _), zip(schema, value))
+
 
 def validate_strict_sequence(schema, value):
     if not len(schema) == len(value):
-        raise ValueError("sequence has a different number of elements than its schema prescribes.  value: {0}, schema: {1}".format(value, schema))
+        raise ValueError(
+            "sequence has a different number of elements than its schema prescribes.  value: {0}, schema: {1}".format(
+                value, schema))
     return map(lambda sub_schema, sub_value: validate_against_schema(sub_schema, sub_value), schema, value)
 
-is_map_template = lambda schema: isinstance(schema, collections.MutableMapping) and len(schema.items()) is 1 and not isinstance(schema.keys()[0], str)
+
+is_map_template = lambda schema: isinstance(schema, collections.MutableMapping) and len(
+    schema.items()) is 1 and not isinstance(schema.keys()[0], str)
 is_keyed_mapping = lambda schema: isinstance(schema, collections.MutableMapping) and not is_map_template(schema)
 is_sequence_template = lambda schema: isinstance(schema, collections.Sequence) and len(schema) is 1
 is_strict_sequence = lambda schema: isinstance(schema, collections.Sequence) and 1 < len(schema)
 
-validate_against_schema = multiple_dispatch_fn( "validate_against_schema", {
+validate_against_schema = multiple_dispatch_fn("validate_against_schema", {
     lambda schema, value: is_sequence_template(schema): validate_sequence_template,
     lambda schema, value: is_strict_sequence(schema): validate_strict_sequence,
     lambda schema, value: is_map_template(schema): validate_map_template,
     lambda schema, value: is_keyed_mapping(schema): validate_keyed_mapping},
-    default=lambda schema, value: schema(value))
+                                               default=lambda schema, value: schema(value))
+
 
 def validator(schema, subject_name_str, validation_predicate=None, coerce_data=False, data=None):
     if data is None:
@@ -52,4 +64,5 @@ def validator(schema, subject_name_str, validation_predicate=None, coerce_data=F
             "value": data,
             "schema": schema
         }
-        raise ValueError("Bad value provided for {subject}. - error: {error} schema: {schema} value: {value}".format(**message_details))
+        raise ValueError("Bad value provided for {subject}. - error: {error} schema: {schema} value: {value}".format(
+            **message_details))
